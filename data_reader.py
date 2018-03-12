1baa681e4413628d029915d06c3bfebc4842e270
Weiqi97 <weltch1997@gmail.com>
2018-03-12 00:04:05 -0400

    Add todos

--- data_reader.py
+++ data_reader.py
@@ -6,7 +6,13 @@ import pandas as pd
 from data_fetcher import ClassNumber, ClassExam, ClassInstructor, ClassConx, \
     SeatInfo
 
+# TODO: See if I want to create a fake function to get rid of stupid warnings
+# TODO: read documentation about queries
+# http://pandas.pydata.org/pandas-docs/stable/indexing.html#indexing-query
 
-final_frame = pd.read_pickle("Test.pkl")
-number = final_frame["number"][0]
-print(number.num)
+final_frame = pd.read_pickle("FINAL_FRAME.pkl")
+result = final_frame.query("CRN == \'10018\'")
+
+title = result['title'].values
+
+print("DONE")
