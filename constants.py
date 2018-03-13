8d41a24c51ab83b2f127a5545d8db149aca7a893
Weiqi97 <weltch1997@gmail.com>
2018-03-13 17:20:42 -0400

    Create constants file

---
+++ constants.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+"""This file contains needed constants."""
+
+from typing import NamedTuple
+
+# Web url constants
+url = "https://weblprod1.wheatonma.edu/PROD/bzcrschd.P_ListSection"
+base_url = "https://weblprod1.wheatonma.edu"
+
+
+# Struct constants
+class ClassNumber(NamedTuple):
+    """Struct for class number information."""
+    num: str
+    link: str
+
+
+class ClassExam(NamedTuple):
+    """Struct for class exam information."""
+    letter: str
+    link: str
+
+
+class ClassInstructor(NamedTuple):
+    """Struct for class instructor information."""
+    name: str
+    link: str
+
+
+class ClassConx(NamedTuple):
+    """Struct for class connection information."""
+    num: str
+    link: str
+
+
+class SeatInfo(NamedTuple):
+    """Struct for seats information."""
+    max: str
+    taken: str
+    avail: str
+    wait_list: str
