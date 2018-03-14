44af4b3d8492d9e27daf39b7272ba6e4d93377b9
Weiqi97 <weltch1997@gmail.com>
2018-03-13 21:39:08 -0400

    Update reader file

--- data_reader.py
+++ data_reader.py
@@ -1,18 +1,15 @@
-
 # coding=utf-8
+"""This file reads the data and select right query based on users request."""
 
 import pandas as pd
-
-from data_fetcher import ClassNumber, ClassExam, ClassInstructor, ClassConx, \
-    SeatInfo
+from constants import ClassConx, ClassInstructor, ClassExam, ClassNumber
 
 # TODO: See if I want to create a fake function to get rid of stupid warnings
 # TODO: read documentation about queries
 # http://pandas.pydata.org/pandas-docs/stable/indexing.html#indexing-query
+from data_processor import save_all_info
 
-final_frame = pd.read_pickle("FINAL_FRAME.pkl")
-result = final_frame.query("CRN == \'10018\'")
+save_all_info()
 
-title = result['title'].values
-
+# final_frame = pd.read_pickle("FINAL_FRAME.pkl")
 print("DONE")
