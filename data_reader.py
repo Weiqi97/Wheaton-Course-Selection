97a8844e086a2428e7538585fcdf84f831bc503d
Weiqi97 <weltch1997@gmail.com>
2018-03-14 19:49:21 -0400

    Ready to merge

--- data_reader.py
+++ data_reader.py
@@ -2,14 +2,13 @@
 """This file reads the data and select right query based on users request."""
 
 import pandas as pd
-from constants import ClassConx, ClassInstructor, ClassExam, ClassNumber
-
+from data_processor import save_all_info
+# from constants import ClassConx, ClassInstructor, ClassExam, ClassNumber
 # TODO: See if I want to create a fake function to get rid of stupid warnings
 # TODO: read documentation about queries
 # http://pandas.pydata.org/pandas-docs/stable/indexing.html#indexing-query
-from data_processor import save_all_info
+
 
 save_all_info()
-
-# final_frame = pd.read_pickle("FINAL_FRAME.pkl")
+final_frame = pd.read_pickle("FINAL_FRAME.pkl")
 print("DONE")
