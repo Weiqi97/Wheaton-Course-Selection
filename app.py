9cb917a485c66937e45ef77b13724d0ee2f030fa
Weiqi97 <weltch1997@gmail.com>
2018-03-14 21:36:59 -0400

    update multiple select

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
 
-from data_fetcher import fetch_semesters
+from data_fetcher import fetch_semesters, fetch_subjects
 
 app = Flask(__name__)
 app.debug = True
 
 
 @app.route("/", methods=["GET"])
-def drop_down():
+def drop_down_selections():
+    subjects = fetch_subjects()
     semesters = fetch_semesters()
     return render_template("test.html",
+                           subjects=subjects,
                            semesters=semesters)
 
 
