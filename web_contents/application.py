1d75912b30fafd50c3179219786ffd0d07860da4
Weiqi97 <weltch1997@gmail.com>
2018-03-16 14:20:02 -0400

    Make application work

--- web_contents/application.py
+++ web_contents/application.py
@@ -7,38 +7,44 @@ app = Flask(__name__)
 app.debug = True
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def drop_down_selections():
-    semesters = fetch_semesters()
-    subjects = fetch_subjects()
-    foundations = fetch_foundations()
-    divisions = fetch_divisions()
-    areas = fetch_areas()
+    fetched_semesters = fetch_semesters()
+    fetched_subjects = fetch_subjects()
+    fetched_foundations = fetch_foundations()
+    fetched_divisions = fetch_divisions()
+    fetched_areas = fetch_areas()
+    
+    if request.method == "GET":
+        return render_template("index.html",
+                               data_generated=False,
+                               fetched_semesters=fetched_semesters,
+                               fetched_subjects=fetched_subjects,
+                               fetched_foundations=fetched_foundations,
+                               fetched_divisions=fetched_divisions,
+                               fetched_areas=fetched_areas)
 
-    return render_template("index.html",
-                           semesters=semesters,
-                           subjects=subjects,
-                           foundations=foundations,
-                           divisions=divisions,
-                           areas=areas)
+    if request.method == "POST":
+        semester = request.form["semester"]
+        subjects = request.form.getlist("subjects")
+        foundation = request.form["foundation"]
+        division = request.form["division"]
+        area = request.form["area"]
 
+        selected_data = read_data(semester=semester,
+                                  subjects=subjects,
+                                  foundation=foundation,
+                                  division=division,
+                                  area=area)
 
-@app.route("/", methods=["POST"])
-def return_table_values():
-    semester = request.form["semester"]
-    subjects = request.form.getlist("subjects")
-    foundation = request.form["foundation"]
-    division = request.form["division"]
-    area = request.form["area"]
-
-    selected_data = read_data(semester=semester,
-                              subjects=subjects,
-                              foundation=foundation,
-                              division=division,
-                              area=area)
-
-    return render_template("index.html",
-                           selected_data=selected_data)
+        return render_template("index.html",
+                               data_generated=True,
+                               fetched_semesters=fetched_semesters,
+                               fetched_subjects=fetched_subjects,
+                               fetched_foundations=fetched_foundations,
+                               fetched_divisions=fetched_divisions,
+                               fetched_areas=fetched_areas,
+                               selected_data=selected_data)
 
 
 if __name__ == "__main__":
