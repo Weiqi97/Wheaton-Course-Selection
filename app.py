cdaba7e9c9ec7b446f391d10739b414508bd8d7e
Phuens <norbu_phuntsho@wheatoncollege.edu>
2018-03-15 21:14:21 -0400

    Front end layout

--- app.py
+++ app.py
@@ -4,22 +4,9 @@ from data_fetcher import fetch_semesters, fetch_subjects
 
 app = Flask(__name__)
 app.debug = True
-
-
-@app.route("/", methods=["GET"])
-def drop_down_selections():
-    subjects = fetch_subjects()
-    semesters = fetch_semesters()
-    return render_template("test.html",
-                           subjects=subjects,
-                           semesters=semesters)
-
-
-@app.route("/", methods=["POST"])
-def sth():
-    semester = request.form["semesters"]
-    subjects = request.form.getlist("subjects")
-    print("DONE")
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
