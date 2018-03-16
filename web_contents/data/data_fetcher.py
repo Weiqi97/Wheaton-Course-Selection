0c22a45b839484119651809b3a5d05487d240ca6
Weiqi97 <weltch1997@gmail.com>
2018-03-15 20:58:21 -0400

    fetch int majors

--- web_contents/data/data_fetcher.py
+++ web_contents/data/data_fetcher.py
@@ -163,3 +163,27 @@ def fetch_areas() -> pd.Series:
     # return the desired values. (Exclude: '%', it means all subject.)
     return pd.Series(data=option_values, index=option_names)
 
+
+def fetch_intmajor() -> pd.Series:
+    """
+    Fetch all existing foundation names.
+    :return: A pandas series, where
+            - index are interdisciplinary major names.
+            - Data are corresponding interdisciplinary major values.
+    """
+    # Set up the fake browser object and open the target website.
+    browser = mechanicalsoup.StatefulBrowser()
+    web_soup = browser.open(url).soup
+
+    # Find the correct select tag.
+    select_box = web_soup.find("select", {"name": "intmajor_sch"})
+
+    # Extract values from the tag.
+    options = select_box.find_all("option")
+
+    # Get values and names.
+    option_values = [option["value"] for option in options]
+    option_names = [str(option.contents[0]) for option in options]
+
+    # return the desired values. (Exclude: '%', it means all subject.)
+    return pd.Series(data=option_values, index=option_names)
