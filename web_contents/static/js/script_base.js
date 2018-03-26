ff375bf8a11ee116414d753a9c81c826124403f6
Weiqi97 <weltch1997@gmail.com>
2018-03-26 14:41:51 -0400

    Split javascript files

---
+++ web_contents/static/js/script_base.js
@@ -0,0 +1,24 @@
+// This function will alert user if they left select box empty.
+function checkField() {
+    var subjects = document.getElementById("subjects").value;
+    var semester = document.getElementById("semester").value;
+    if (subjects === "") {
+        swal({
+            type: "warning",
+            title: "Please select subject(s)!",
+            confirmButtonText: "Got it!"
+        });
+        return false;
+    }
+    else if (semester === "") {
+        swal({
+            type: "warning",
+            title: "Please select a semester!",
+            confirmButtonText: "Got it!"
+        });
+        return false;
+    }
+    else{
+        return true;
+    }
+}
