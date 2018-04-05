bde82b57bfcca38a4d67954c77829b42d1f7839d
Weiqi97 <weltch1997@gmail.com>
2018-04-05 00:47:01 -0400

    Move function and commenting

--- web_contents/static/js/script_base.js
+++ web_contents/static/js/script_base.js
@@ -1,7 +1,32 @@
+// Global constants
+var height_adjust = 200; // Adjust the table size.
+
+
 // Function to resize data table every time browser size changes.
 $(function() {
     $(window).on("resize", resize_result_table);
 } );
+
+function resize_result_table() {
+    // Get the new proper window height.
+    var window_height = window.innerHeight-height_adjust;
+
+    // Reinitialize data table to a proper size.
+    $('#result_table').DataTable( {
+        "destroy":        true,
+        "scrollY":        window_height,
+        "scrollX":        true,
+        "scrollCollapse": true,
+        "paging":         false,
+        "bInfo" :         false,
+        "columnDefs": [
+            {
+                "targets": [6, 7, 8, 9, 10, 11, 12, 13, 14],
+                "visible": false
+            }
+        ]
+    } );
+}
 
 // TODO: If possible make the alert box bigger with larger font.
 // This function will alert user if they left select box empty.
