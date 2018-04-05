74796b155c15ce0a876ac18867e8683f7a3b9147
Weiqi97 <weltch1997@gmail.com>
2018-04-05 02:12:51 -0400

    Reorganizing javascript

--- web_contents/static/js/script_base.js
+++ web_contents/static/js/script_base.js
@@ -1,4 +1,42 @@
+// Global constants
+var height_adjust = 200; // Adjust the table size.
 
+// When browser size changed, find proper size of new data table.
+function resize_result_table() {
+    // Get the new proper window height.
+    var window_height = window.innerHeight-height_adjust;
+
+    // Reinitialize data table to a proper size.
+    $('#result_table').DataTable( {
+        "destroy":        true,
+        "scrollY":        window_height,
+        "scrollX":        true,
+        "scrollCollapse": true,
+        "paging":         false,
+        "bInfo" :         false,
+        "columnDefs": [
+            {
+                "targets": [6, 7, 8, 9, 10, 11, 12, 13, 14],
+                "visible": false
+            }
+        ]
+    } );
+}
+
+// Format result to display as a table in row child.
+function format(CRN, Exam, Connection, Location, Textbook, Info, Seats, Special) {
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+        '<tr>'+
+            '<td width="25%">'+CRN+'</td>'+
+            '<td width="25%">'+Exam+'</td>'+
+            '<td width="25%">'+Location+'</td>'+
+            '<td width="25%">'+Textbook+'</td>'+
+        '</tr>'+
+        '<tr>'+Info+'</tr>'+
+        '<tr>'+Seats+'</tr>'+
+        '<tr>'+Special+'</tr>'+
+        '</table>';
+}
 
 // TODO: If possible make the alert box bigger with larger font.
 // This function will alert user if they left select box empty.
