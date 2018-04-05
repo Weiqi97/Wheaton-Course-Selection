29bba6830463afdfd562a9d61debe63beac61706
Weiqi97 <weltch1997@gmail.com>
2018-04-05 00:02:07 -0400

    Commenting

--- web_contents/static/js/script_base.js
+++ web_contents/static/js/script_base.js
@@ -1,7 +1,12 @@
+
+// TODO: If possible make the alert box bigger with larger font.
 // This function will alert user if they left select box empty.
 function checkField() {
+    // Get values of the subject select and semester select.
     var subjects = document.getElementById("subjects").value;
     var semester = document.getElementById("semester").value;
+
+    // Check if subject was empty, if so alert with proper message.
     if (subjects === "") {
         swal({
             type: "warning",
@@ -10,6 +15,8 @@ function checkField() {
         });
         return false;
     }
+
+    // Check if subject was empty, if so alert with proper message.
     else if (semester === "") {
         swal({
             type: "warning",
@@ -22,5 +29,5 @@ function checkField() {
         return true;
     }
 }
-// language=JQuery-CSS
+
 
