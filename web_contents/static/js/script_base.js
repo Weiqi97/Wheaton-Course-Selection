d87622feb573ebb7b84011fab38a287aab710569
Weiqi97 <weltch1997@gmail.com>
2018-04-05 01:23:29 -0400

    Function removal

--- web_contents/static/js/script_base.js
+++ web_contents/static/js/script_base.js
@@ -1,32 +1,4 @@
-// Global constants
-var height_adjust = 200; // Adjust the table size.
 
-
-// Function to resize data table every time browser size changes.
-$(function() {
-    $(window).on("resize", resize_result_table);
-} );
-
-function resize_result_table() {
-    // Get the new proper window height.
-    var window_height = window.innerHeight-height_adjust;
-
-    // Reinitialize data table to a proper size.
-    $('#result_table').DataTable( {
-        "destroy":        true,
-        "scrollY":        window_height,
-        "scrollX":        true,
-        "scrollCollapse": true,
-        "paging":         false,
-        "bInfo" :         false,
-        "columnDefs": [
-            {
-                "targets": [6, 7, 8, 9, 10, 11, 12, 13, 14],
-                "visible": false
-            }
-        ]
-    } );
-}
 
 // TODO: If possible make the alert box bigger with larger font.
 // This function will alert user if they left select box empty.
@@ -59,7 +31,3 @@ function checkField() {
     }
 }
 
-// A helper function for replace all.
-function replaceAll(str, find, replace) {
-    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
-}
