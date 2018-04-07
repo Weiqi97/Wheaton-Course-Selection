80492e8bcec18b900f93a6da6ce939a9fbcbb6cc
Weiqi97 <weltch1997@gmail.com>
2018-04-07 17:14:42 -0400

    animation for data table child row

--- static/js/script_base.js
+++ static/js/script_base.js
@@ -1,16 +1,17 @@
 // Format result to display as a table in row child.
 function format(CRN, Exam, Connection, Location, Textbook, Info, Seats, Special) {
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-        '<tr>'+
-            '<td width="25%">'+CRN+'</td>'+
-            '<td width="25%">'+Exam+'</td>'+
-            '<td width="25%">'+Location+'</td>'+
-            '<td width="25%">'+Textbook+'</td>'+
-        '</tr>'+
-        '<tr>'+Info+'</tr>'+
-        '<tr>'+Seats+'</tr>'+
-        '<tr>'+Special+'</tr>'+
-        '</table>';
+    return '<div class="slider">'+
+        '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+            '<tr>'+
+                '<td width="25%">'+CRN+'</td>'+
+                '<td width="25%">'+Exam+'</td>'+
+                '<td width="25%">'+Location+'</td>'+
+                '<td width="25%">'+Textbook+'</td>'+
+            '</tr>'+
+            '<tr>'+Info+'</tr>'+
+            '<tr>'+Seats+'</tr>'+
+            '<tr>'+Special+'</tr>'+
+        '</table></div>';
 }
 
 // TODO: If possible make the alert box bigger with larger font.
