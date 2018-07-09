f0d3360e2f2afb007a46cf36a1117adcecad1391
Weiqi97 <weltch1997@gmail.com>
2018-07-09 00:46:01 -0400

    Pydocstyle checks

--- contents/data/data_reader.py
+++ contents/data/data_reader.py
@@ -6,9 +6,8 @@ import pandas as pd
 
 def read_data(semester: str, subjects: list, foundation: str, division: str,
               area: str) -> pd.DataFrame:
-    """
-    This function will reads data from saved file and return proper values
-    based on users request.
+    """Read data from saved file and return values based on users request.
+
     :param semester: User selected semester value.
     :param subjects: User selected subject(s).
     :param foundation: User selected foundation.
@@ -16,9 +15,11 @@ def read_data(semester: str, subjects: list, foundation: str, division: str,
     :param area: User selected area.
     :return: A pandas data frame that contains desired information.
     """
+    # Get the data frame with all saved information.
     data_frame = \
         pd.read_pickle(f"contents/data/course_data/pickle_data/{semester}.pkl")
 
+    # If choose all subject, pass, otherwise select desired rows.
     if "%" in subjects:
         pass
     else:
@@ -26,49 +27,54 @@ def read_data(semester: str, subjects: list, foundation: str, division: str,
                        for subject in subjects]
         data_frame = pd.DataFrame(pd.concat(data_frames))
 
+    # If choose all foundation, pass, otherwise select desired rows.
     if foundation == "%":
         pass
     else:
         data_frame = data_frame.loc[data_frame["foundation"] == foundation]
 
+    # If choose all divisions, pass, otherwise select desired rows.
     if division == "%":
         pass
     else:
         data_frame = data_frame.loc[data_frame["division"] == division]
 
+    # If choose all areas, pass, otherwise select desired rows.
     if area == "%":
         pass
     else:
         data_frame = data_frame.loc[data_frame["area"] == area]
 
+    # Return refined data frame.
     return data_frame
 
 
 def read_subjects() -> pd.Series:
-    """Reads data from saved pickle file."""
+    """Read data from saved pickle file."""
     return pd.read_pickle("contents/data/web_data/subjects.pkl")
 
 
 def read_semesters() -> pd.Series:
-    """Reads data from saved pickle file."""
+    """Read data from saved pickle file."""
     return pd.read_pickle("contents/data/web_data/semesters.pkl")
 
 
 def read_foundations() -> pd.Series:
-    """Reads data from saved pickle file."""
+    """Read data from saved pickle file."""
     return pd.read_pickle("contents/data/web_data/foundations.pkl")
 
 
 def read_divisions() -> pd.Series:
-    """Reads data from saved pickle file."""
+    """Read data from saved pickle file."""
     return pd.read_pickle("contents/data/web_data/divisions.pkl")
 
 
 def read_areas() -> pd.Series:
-    """Reads data from saved pickle file."""
+    """Read data from saved pickle file."""
     return pd.read_pickle("contents/data/web_data/areas.pkl")
 
 
 def read_current_semester() -> str:
-    """Reads data from saved pickle file."""
+    """Read data from saved pickle file."""
+    # The list always contains only one element, grab it.
     return pd.read_pickle("contents/data/web_data/current_semester.pkl")[0]
