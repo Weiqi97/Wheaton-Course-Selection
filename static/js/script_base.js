11e813e1d235b55e49a4d72a50767f8300c94703
Weiqi97 <weltch1997@gmail.com>
2018-07-12 07:05:32 -0400

    Split data table child row method

--- static/js/script_base.js
+++ static/js/script_base.js
@@ -4,107 +4,68 @@ import * as classTable from './class_table.js'
 
 /**
  * This function formats the given information as a table in row child.
- * @param CRN {string} - The refined CRN number
- * @param Exam {string} - Exam time.
- * @param Connection {string} - Connection.
- * @param Location {string} - Class location.
- * @param Textbook {string} - Textbook with its link.
- * @param Info {string} - Foundation, division, area.
- * @param Seats {string} - Seats info.
- * @param Special {string} - Special info.
+ * @param {Array.<string>} rowData: list of data from the row.
  * @returns {string}
  */
-function format(CRN, Exam, Connection, Location, Textbook, Info, Seats, Special) {
-  // TODO: use `text ${var}` to get element. (string literal)
-  return '<div class="slider">' +
-    '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
-    '<tr>' +
-    '<td width="25%">' + CRN + '</td>' +
-    '<td width="25%">' + Exam + '</td>' +
-    '<td width="25%">' + Location + '</td>' +
-    '<td width="25%">' + Textbook + '</td>' +
-    '</tr>' +
-    '<tr>' + Info + '</tr>' +
-    '<tr>' + Seats + '</tr>' +
-    '<tr>' + Special + '</tr>' +
-    '</table></div>';
+function format(rowData) {
+  // Format some information ahead of time.
+  const exam = rowData[9] === '' ? '' : `Exam: ${rowData[9]}`;
+  const area = rowData[12] === '' ? '<td></td>' : `<td>Area: ${rowData[12]}</td>`;
+  const division = rowData[11] === '' ? '<td></td>' : `<td>Division: ${rowData[11]}</td>`;
+  const foundation = rowData[10] === '' ? '<td></td>' : `<td>Foundation: ${rowData[10]}</td>`;
+  const connection = rowData[13] === '' ? '<td></td>' : `<td>Connection: ${rowData[13]}</td>`;
+  //TODO: formatting needs much more work.
+  return `
+      <div class="slider">
+          <table cellpadding="0" cellspacing="0" border="0">
+              <tr>
+                  <td width="25%">CRN: ${rowData[6]}</td>
+                  <td width="25%">${exam}</td>
+                  <td width="25%">Location: ${rowData[7]}</td>
+                  <td width="25%">Textbook: ${rowData[15]}</td>
+              </tr>
+              <tr>
+                  ${area}${division}${foundation}${connection}
+              </tr>
+              <tr>
+                  <td colspan="4">${rowData[14]}</td>
+              </tr>
+              <tr>
+                  <td colspan="4">Note: ${rowData[16]}</td>
+              </tr>
+          </table>
+      </div>`
 }
 
+function expandDataRow() {
+  // Get the data table selector.
+  const courseTable = $('#course-table');
+  // Get the data table object.
+  const dataTable = courseTable.DataTable();
+  // Get the jQuery table object.
+  const jQueryTable = courseTable.dataTable();
+  // Find the closest tr.
+  const tr = $(event.currentTarget).closest('tr');
+  // Format the row data.
+  const formattedRow = format(jQueryTable.fnGetData(tr));
+  // Get the data table row.
+  const row = dataTable.row(tr);
+
+  // Check if row child is shown. If it is, hide it.
+  if (row.child.isShown()) {
+    $('div.slider', row.child()).slideUp(function () {
+      row.child.hide();
+      tr.removeClass("shown");
+    });
+  } else {
+    // If not shown, display it.
+    row.child(formattedRow, "no-padding").show();
+    tr.addClass("shown");
+    $('div.slider', row.child()).slideDown();
+  }
 
 
-
-//
-// /**
-//  * Display the data table while hiding the row child.
-//  */
-// $(function dataTableReady() {
-//   var data_table = $("#result_table");
-//
-//   // Initialize data table.
-//   var table = data_table.DataTable({
-//     "scrollY": 643,
-//     "scrollX": true,
-//     "scrollCollapse": true,
-//     "paging": false,
-//     "bSort": false,
-//     "bInfo": false,
-//     "sDom": 'l<"toolbar">frtip',
-//     "language": {
-//       "emptyTable": "No class meets your expectation."
-//     },
-//     "columnDefs": [{
-//       "targets": [6, 7, 8, 9, 10, 11, 12, 13, 14],
-//       "visible": false
-//     }]
-//   });
-//
-//   // Get proper data for row child.
-//   data_table.find("tbody").on("click", "#show_detail", function () {
-//     // Set variables for row child display method.
-//     var tr = $(this).closest("tr");
-//     var row = table.row(tr);
-//     // Get all the data from table row.
-//     var CRN = table.row(this).data()[6];
-//     var Exam = table.row(this).data()[7];
-//     var Connection = table.row(this).data()[8];
-//     var Location = table.row(this).data()[9];
-//     var Textbook = table.row(this).data()[10];
-//     var Info = table.row(this).data()[11];
-//     var Seats = table.row(this).data()[12];
-//     var Special = table.row(this).data()[13];
-//
-//     // TODO: UGLY. Seeking for fixing method afterwards.
-//     // Refine info strings and add connection to it.
-//     Info = Info.replace(/_/g, "<td>");
-//     Info = Info.replace(/!/g, "</td>");
-//     if (Connection !== "")
-//       Info = Info + "<td>" + Connection + "</td>";
-//
-//     // Refine seats string.
-//     Seats = Seats.replace(/_/g, "<td>");
-//     Seats = Seats.replace(/!/g, "</td>");
-//
-//     // Refine special info string.
-//     Special = Special.replace(/_/g, '<td colspan="4"><div>');
-//     Special = Special.replace(/!/g, '</td>');
-//
-//     // This row is already open - close it.
-//     if (row.child.isShown()) {
-//       $("div.slider", row.child()).slideUp(function () {
-//         row.child.hide();
-//         tr.removeClass("shown");
-//       });
-//     }
-//     // Open this row.
-//     else {
-//       row.child(format(CRN, Exam, Connection, Location, Textbook,
-//         Info, Seats, Special), "no-padding").show();
-//       tr.addClass("shown");
-//       $('div.slider', row.child()).slideDown();
-//     }
-//   });
-// });
-//
+}
 
 /**
  * Get all class information.
@@ -119,6 +80,7 @@ function getClassTable() {
         tableHolder.html(response);
         classTable.convertDataTable(tableHolder.children());
         $('.to-calendar').click(calendar.toCalendar);
+        $('.show-detail').click(expandDataRow)
       }
     )
     .fail( // If something went wrong.
