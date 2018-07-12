25dc6bcbcd5c90ea83fb5423f3055668c863fb5a
Weiqi97 <weltch1997@gmail.com>
2018-07-12 05:22:40 -0400

    Remove functions -- refactor

--- static/js/script_base.js
+++ static/js/script_base.js
@@ -1,4 +1,5 @@
 import * as utility from './utility.js'
+import * as calendar from './calendar.js'
 import * as classTable from './class_table.js'
 
 /**
@@ -30,45 +31,7 @@ function format(CRN, Exam, Connection, Location, Textbook, Info, Seats, Special)
 }
 
 
-/**
- * Convert time to proper format to display.
- * @param time {string} the refined time.
- * @returns {string} time in format of "HH:MM:SS"
- */
-function timeConverter(time) {
-  if (time.includes('A')) {
-    if (time[2] === ':')
-      return `${time.slice(0, 5)}:00`;
-    else
-      return `0${time.slice(0, 4)}:00`;
-  }
-  else {
-    if (Number(time.slice(0, 2)) !== 12) {
-      return `${Number(time.slice(0, 1)) + 12}${time.slice(1, 4)}:00`;
-    }
-    return `${time.slice(0, 5)}:00`;
-  }
-}
 
-
-/**
- * Convert day to proper format to display
- * @param day {string} the refined day.
- * @returns {string} The corresponding date.
- */
-function dayConverter(day) {
-  if (day === 'M') {
-    return '2018-04-02T';
-  } else if (day === 'T') {
-    return '2018-04-03T';
-  } else if (day === 'W') {
-    return '2018-04-04T';
-  } else if (day === 'R') {
-    return '2018-04-05T';
-  } else if (day === 'F') {
-    return '2018-04-06T'
-  }
-}
 
 //
 // /**
@@ -142,83 +105,6 @@ function dayConverter(day) {
 //   });
 // });
 //
-/**
- * Display the empty calendar.
- */
-$(function readyCalendar() {
-  // --------- Calendar ---------
-  $("#calendar").fullCalendar({
-    height: 740,
-    header: {left: "", center: "", right: ""},
-    // Display just full length of weekday, without dates.
-    columnFormat: 'dddd',
-    defaultView: 'agendaWeek',
-    hiddenDays: [0, 6],    // hide Saturday and Sunday
-    weekNumbers: false,  // don't show week numbers
-    minTime: '8:00:00',   // display from 8 to 22
-    maxTime: '23:00:00',
-    slotDuration: '00:30:00', // 15 minutes for each row
-    allDaySlot: false,        // don't show "all day" at the top
-    editable: true,
-    defaultDate: moment('2018-04-02'),
-    eventStartEditable: false,
-    eventDurationEditable: false,
-
-    // Delete events on click.
-    eventRender: function (event, element) {
-      element.find(".fc-bg").css("pointer-events", "none");
-      element.append(
-        `<div style='position: absolute; bottom: 1px; right: 2px'>
-             <span id='deleteEvent'>
-                 <i  class='fas fa-trash-alt fa-lg' style='color: orangered'></i>
-             </span>
-         </div>`
-      );
-      element.find("#deleteEvent").click(function () {
-        $('#calendar').fullCalendar('removeEvents', event._id);
-      });
-    }
-
-  });
-});
-
-
-/**
- * Add class from data table to calendar.
- * @returns {void}: This function has no return.
- */
-function toCalendar() {
-  // Get the row with the add button clicked.
-  const clickedRow = $(event.currentTarget).closest('tr');
-  // Get the data within the row.
-  const rowData = $('#course-table').dataTable().fnGetData(clickedRow);
-  // Unpack data from the row, we need title and time only.
-  const courseTitle = rowData[4];
-  const courseTime = rowData[5];
-
-  // Check if the class has an assigned time.
-  if (courseTime === "TBA") {
-    utility.classTimeTBAError()
-  } else {
-    // Split times. It is possible that one class has two different times.
-    const timeList = courseTime.split("<br>");
-    // Use a map function to fill all class times in the calendar.
-    timeList.map(function (time) {
-      const days = time.replace(/AM|PM|[0-9]|-|:|\s/g, '').split('');
-      const startEndTime = time.replace(/[MTWRF]|\s/g, '').split('-');
-      for (let day of days) {
-        const newEvent = {
-          id: courseTitle,
-          title: courseTitle,
-          start: dayConverter(day) + timeConverter(startEndTime[0]),
-          end: dayConverter(day) + timeConverter(startEndTime[1]),
-          allDay: false
-        };
-        $("#calendar").fullCalendar('renderEvent', newEvent, 'stick');
-      }
-    });
-  }
-}
 
 /**
  * Get all class information.
@@ -232,7 +118,7 @@ function getClassTable() {
         const tableHolder = $('#course-container');
         tableHolder.html(response);
         classTable.convertDataTable(tableHolder.children());
-        $('.to-calendar').click(toCalendar);
+        $('.to-calendar').click(calendar.toCalendar);
       }
     )
     .fail( // If something went wrong.
@@ -267,6 +153,7 @@ function fillHiddenData() {
 $(function () {
   // When document finishes loading, fill in default data.
   // Then get all classes by default.
+  calendar.readyCalendar();
   fillHiddenData();
   getClassTable();
 
