c5dc2185ec34082e60f441d1fce51c30b4950813
Weiqi97 <weltch1997@gmail.com>
2018-07-13 01:40:11 -0400

    Refactor

--- static/js/script_base.js
+++ static/js/script_base.js
@@ -3,71 +3,6 @@ import * as calendar from './calendar.js'
 import * as classTable from './class_table.js'
 
 /**
- * This function formats the given information as a table in row child.
- * @param {Array.<string>} rowData: list of data from the row.
- * @returns {string}
- */
-function format(rowData) {
-  // Format some information ahead of time.
-  const exam = rowData[9] === '' ? '' : `Exam: ${rowData[9]}`;
-  const area = rowData[12] === '' ? '<td></td>' : `<td>Area: ${rowData[12]}</td>`;
-  const division = rowData[11] === '' ? '<td></td>' : `<td>Division: ${rowData[11]}</td>`;
-  const foundation = rowData[10] === '' ? '<td></td>' : `<td>Foundation: ${rowData[10]}</td>`;
-  const connection = rowData[13] === '' ? '<td></td>' : `<td>Connection: ${rowData[13]}</td>`;
-  //TODO: formatting needs much more work.
-  return `
-      <div class="slider">
-          <table cellpadding="0" cellspacing="0" border="0">
-              <tr>
-                  <td width="25%">CRN: ${rowData[6]}</td>
-                  <td width="25%">${exam}</td>
-                  <td width="25%">Location: ${rowData[7]}</td>
-                  <td width="25%">Textbook: ${rowData[15]}</td>
-              </tr>
-              <tr>
-                  ${area}${division}${foundation}${connection}
-              </tr>
-              <tr>
-                  <td colspan="4">${rowData[14]}</td>
-              </tr>
-              <tr>
-                  <td colspan="4">Note: ${rowData[16]}</td>
-              </tr>
-          </table>
-      </div>`
-}
-
-function expandDataRow() {
-  // Get the data table selector.
-  const courseTable = $('#course-table');
-  // Get the data table object.
-  const dataTable = courseTable.DataTable();
-  // Get the jQuery table object.
-  const jQueryTable = courseTable.dataTable();
-  // Find the closest tr.
-  const tr = $(event.currentTarget).closest('tr');
-  // Format the row data.
-  const formattedRow = format(jQueryTable.fnGetData(tr));
-  // Get the data table row.
-  const row = dataTable.row(tr);
-
-  // Check if row child is shown. If it is, hide it.
-  if (row.child.isShown()) {
-    $('div.slider', row.child()).slideUp(function () {
-      row.child.hide();
-      tr.removeClass('shown');
-    });
-    $(event.currentTarget).html(`<i class="fas fa-angle-double-down fa-lg"></i>`);
-  } else {
-    // If not shown, display it.
-    row.child(formattedRow, 'no-padding').show();
-    tr.addClass('shown');
-    $('div.slider', row.child()).slideDown();
-    $(event.currentTarget).html(`<i class="fas fa-angle-double-up fa-lg"></i>`);
-  }
-}
-
-/**
  * Get all class information.
  * @returns {void}: This function has no return.
  */
@@ -80,14 +15,14 @@ function getClassTable() {
         tableHolder.html(response);
         classTable.convertDataTable(tableHolder.children());
         $('.to-calendar').click(calendar.toCalendar);
-        $('.show-detail').click(expandDataRow)
+        $('.show-detail').click(classTable.expandDataRow);
       }
     )
     .fail( // If something went wrong.
       function () {
         $.alert({
           type: `red`,
-          icon: 'fa fa-warning',
+          icon: 'fas fa-exclamation-triangle',
           theme: 'modern',
           title: 'Error!',
           content: 'Something went wrong while getting the class information, please try again later.'
@@ -95,9 +30,6 @@ function getClassTable() {
       }
     )
 }
-
-
-
 
 /**
  * Run these functions when HTML finish loading.
@@ -117,7 +49,7 @@ $(function () {
       utility.subjectsError();
     } else {
       // Fill in users selection and refine classes.
-      fillHiddenData();
+      utility.fillHiddenData();
       getClassTable();
     }
   })
