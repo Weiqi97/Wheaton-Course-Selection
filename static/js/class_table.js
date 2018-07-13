1015658a8738e96decf4abb2a8e05de4b031ff64
Weiqi97 <weltch1997@gmail.com>
2018-07-13 01:39:45 -0400

    Refactor function

--- static/js/class_table.js
+++ static/js/class_table.js
@@ -1,4 +1,39 @@
 /**
+ * This function formats the given information as a table in row child.
+ * @param {Array.<string>} rowData: list of data from the row.
+ * @returns {string}
+ */
+function format(rowData) {
+  // Format some information ahead of time.
+  const exam = rowData[9] === '' ? '' : `Exam: ${rowData[9]}`;
+  const area = rowData[12] === '' ? '<td></td>' : `<td>Area: ${rowData[12]}</td>`;
+  const division = rowData[11] === '' ? '<td></td>' : `<td>Division: ${rowData[11]}</td>`;
+  const foundation = rowData[10] === '' ? '<td></td>' : `<td>Foundation: ${rowData[10]}</td>`;
+  const connection = rowData[13] === '' ? '<td></td>' : `<td>Connection: ${rowData[13]}</td>`;
+  //TODO: formatting needs much more work.
+  return `
+      <div class="slider">
+          <table cellpadding="0" cellspacing="0" border="0">
+              <tr>
+                  <td width="25%">CRN: ${rowData[6]}</td>
+                  <td width="25%">${exam}</td>
+                  <td width="25%">Location: ${rowData[7]}</td>
+                  <td width="25%">Textbook: ${rowData[15]}</td>
+              </tr>
+              <tr>
+                  ${area}${division}${foundation}${connection}
+              </tr>
+              <tr>
+                  <td colspan="4">${rowData[14]}</td>
+              </tr>
+              <tr>
+                  <td colspan="4">Note: ${rowData[16]}</td>
+              </tr>
+          </table>
+      </div>`
+}
+
+/**
  * Convert HTML table to data table.
  * @param {string} table: HTML table that needs to be converted to data table.
  * @returns {void}: This function has no return.
@@ -27,4 +62,38 @@ export function convertDataTable(table) {
       }
     ]
   });
+}
+
+/**
+ * This function sets expand data row when click.
+ * @returns {void}: This function has no return.
+ */
+export function expandDataRow() {
+  // Get the data table selector.
+  const courseTable = $('#course-table');
+  // Get the data table object.
+  const dataTable = courseTable.DataTable();
+  // Get the jQuery table object.
+  const jQueryTable = courseTable.dataTable();
+  // Find the closest tr.
+  const tr = $(event.currentTarget).closest('tr');
+  // Format the row data.
+  const formattedRow = format(jQueryTable.fnGetData(tr));
+  // Get the data table row.
+  const row = dataTable.row(tr);
+
+  // Check if row child is shown. If it is, hide it.
+  if (row.child.isShown()) {
+    $('div.slider', row.child()).slideUp(function () {
+      row.child.hide();
+      tr.removeClass('shown');
+    });
+    $(event.currentTarget).html(`<i class="fas fa-angle-down fa-lg" style="color: #EE6551"></i>`);
+  } else {
+    // If not shown, display it.
+    row.child(formattedRow, 'no-padding').show();
+    tr.addClass('shown');
+    $('div.slider', row.child()).slideDown();
+    $(event.currentTarget).html(`<i class="fas fa-angle-up fa-lg" style="color: orangered;"></i>`);
+  }
 }
