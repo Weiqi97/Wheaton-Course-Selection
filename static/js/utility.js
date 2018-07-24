302a4b2b5eeb5b856eec0d974103b9b5a007c5fe
Weiqi97 <weltch1997@gmail.com>
2018-07-24 12:37:11 -0400

    All fits newest eslint standards

--- static/js/utility.js
+++ static/js/utility.js
@@ -1,22 +1,22 @@
 /**
  * The function to convert the form into json.
- * @returns {{string: string}}: the form converted to json.
+ * @returns {{string: string}} - The form converted to json.
  */
-export function jsonifyForm() {
-  const form = {};
+export function jsonifyForm () {
+  const form = {}
   $.each($('form').serializeArray(), function (i, field) {
     form[field.name] = field.value || ''
-  });
+  })
   return form
 }
 
 /**
  * Send the ajax request.
- * @param {string} url: the url to post.
- * @param {{string: string}} form: the form data packed into an object.
- * @returns {jQuery.Ajax}: an jQuery Ajax object.
+ * @param {string} url - The url to post.
+ * @param {{string: string}} form - The form data packed into an object.
+ * @returns {jQuery.Ajax} - An jQuery Ajax object.
  */
-export function sendAjaxRequest(url, form) {
+export function sendAjaxRequest (url, form) {
   return $.ajax({
     url: url,
     type: 'POST',
@@ -27,32 +27,32 @@ export function sendAjaxRequest(url, form) {
 
 /**
  * Fill user's selection into a hidden form for back end.
- * @returns {void}: This function has no return.
+ * @returns {void} - This function has no return.
  */
-export function fillHiddenData() {
-  $('#area-value').val($('#area').val());
-  $('#division-value').val($('#division').val());
-  $('#semester-value').val($('#semester').val());
-  $('#subjects-value').val($('#subjects').val());
-  $('#foundation-value').val($('#foundation').val());
+export function fillHiddenData () {
+  $('#area-value').val($('#area').val())
+  $('#division-value').val($('#division').val())
+  $('#semester-value').val($('#semester').val())
+  $('#subjects-value').val($('#subjects').val())
+  $('#foundation-value').val($('#foundation').val())
 }
 
 /**
  * Check if any subject was selected.
- * @returns {boolean}: True if no subject selected, otherwise false.
+ * @returns {boolean} - True if no subject selected, otherwise false.
  */
-export function checkSelectedSubjects() {
+export function checkSelectedSubjects () {
   // Get selected subjects from the drop down.
-  const subjects = $('#subjects').val();
+  const subjects = $('#subjects').val()
   // Check if no subject was selected.
-  return subjects.length === 0;
+  return subjects.length === 0
 }
 
 /**
  * Alert users that no subject was selected.
- * @returns {void}: This function has no return.
+ * @returns {void} - This function has no return.
  */
-export function subjectsError() {
+export function subjectsError () {
   // Specify the alert details.
   $.confirm({
     type: 'red',
@@ -71,9 +71,9 @@ export function subjectsError() {
 
 /**
  * Alert users when then add a class with no assigned time to calendar.
- * @returns {void}: This function has no return.
+ * @returns {void} - This function has no return.
  */
-export function classTimeTBAError() {
+export function classTimeTBAError () {
   // Specify the alert details.
   $.confirm({
     type: 'red',
